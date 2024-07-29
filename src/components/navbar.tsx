935e7a761f0d3fca4a6bb6a73c297fa16b09f698
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2024-07-29 20:46:40 +0530

    minor fixes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,31 @@
-
 import Link from "next/link";
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
 
-const LocalNavbar= () => {
+const LocalNavbar = () => {
   return (
     <Navbar fluid className="bg-black text-white text-xl font-semibold p-4">
       <NavbarBrand as={Link} href="#">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Covered .</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Covered .
+        </span>
       </NavbarBrand>
       <NavbarToggle />
       <NavbarCollapse>
-        <NavbarLink href="#" className="text-white">Pricing</NavbarLink>
-        <NavbarLink href="#" className="text-white">About me</NavbarLink>
+        <NavbarLink href="#" className="text-white">
+          Pricing
+        </NavbarLink>
+        <NavbarLink href="#" className="text-white">
+          About me
+        </NavbarLink>
       </NavbarCollapse>
     </Navbar>
   );
-}
+};
 
 export default LocalNavbar;
